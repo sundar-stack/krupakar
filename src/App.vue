879c0ae17f449d9bb1d2c9f957cdf4ecb5e304fe
sundar <sundar.potlacheruvu@engro.io>
2023-05-23 09:40:49 +0530

    f

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MenuBar from "./components/MenuBar.vue";
+import '../styles/global.css';
+</script>
+<template>
+  <v-app>
+    <!-- <MenuBar :key="$route.fullPath" /> -->
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,187 @@
+<script setup>
+import { onMounted } from "vue";
+import { ref, toRaw } from "vue";
+import { useRouter } from "vue-router";
+import UserServices from "../services/UserServices.js";
+
+const router = useRouter();
+const isCreateAccount = ref(false);
+const snackbar = ref({
+  value: false,
+  color: "",
+  text: "",
+});
+const user = ref({
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+});
+
+onMounted(async () => {
+  if (localStorage.getItem("user") !== null) {
+    router.push({ name: "recipes" });
+  }
+});
+
+function navigateToRecipes() {
+  router.push({ name: "recipes" });
+}
+
+async function createAccount() {
+  await UserServices.addUser(user.value)
+    .then(() => {
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = "Account created successfully!";
+      router.push({ name: "login" });
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
+}
+
+async function login() {
+  console.log(user.value);
+  await UserServices.loginUser(user)
+    .then((data) => {
+      window.localStorage.setItem("user", JSON.stringify(data.data));
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = "Login successful!";
+      router.push({ name: "recipes" });
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
+}
+
+function openCreateAccount() {
+  isCreateAccount.value = true;
+}
+
+function closeCreateAccount() {
+  isCreateAccount.value = false;
+}
+
+function closeSnackBar() {
+  snackbar.value.value = false;
+}
+</script>
+
+<template>
+  <section class="login_wrapper">
+    <div class="login_form">
+      <v-card class="rounded-lg elevation-5">
+        <v-card-title class="heading_secondary text_align_center color_white"
+          >Login
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="user.email"
+            label="Email"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="user.password"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn variant="flat" color="secondary" @click="openCreateAccount()"
+            >Dont have an Account ?</v-btn
+          >
+          <v-spacer></v-spacer>
+
+          <v-btn variant="flat" color="primary" @click="login()">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <v-dialog persistent v-model="isCreateAccount" width="800">
+        <v-card class="rounded-lg elevation-5">
+          <v-card-title class="heading_secondary text_align_center color_grey">Create Account </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="user.firstName"
+              label="First Name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="user.lastName"
+              label="Last Name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="user.email"
+              label="Email"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="user.password"
+              label="Password"
+              required
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              variant="flat"
+              color="secondary"
+              @click="closeCreateAccount()"
+              >Already have an Account ?</v-btn
+            >
+            <v-btn variant="flat" color="primary" @click="createAccount()"
+              >Create Account</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-snackbar v-model="snackbar.value" rounded="pill">
+        {{ snackbar.text }}
+
+        <template v-slot:actions>
+          <v-btn
+            :color="snackbar.color"
+            variant="text"
+            @click="closeSnackBar()"
+          >
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </div>
+  </section>
+</template>
+<style>
+.login_wrapper {
+  width: 100%;
+  height: 100dvh;
+  display: grid;
+  place-items: center;
+  background: url("https://images.unsplash.com/photo-1464618663641-bbdd760ae84a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80");
+  background-size: cover;
+  background-position: center;
+}
+.login_form {
+  width: 55%;
+}
+
+.login_form .v-card {
+  border: 2px solid #ffff;
+  border-radius: 5px;
+  background: #ffffff25;
+  backdrop-filter: blur(5px);
+}
+</style>
